418c00a8454013d5dc2e7d1eff6ab121b06c46ba
Stephan van Ellewee <stephan.van.ellewee@gmail.com>
2013-05-01 06:37:26 +0200

    got a square, working on cpp skeleton Use: g++ bresenham.cpp -o test $(sdl-config --libs)

--- bresenham.cpp
+++ bresenham.cpp
@@ -14,15 +14,123 @@
 
 SDL_Surface *screen;
 
-void putpixel(int x, int y, int color)
+typedef struct Coord{
+  int x;
+  int y;
+};
+
+
+
+void putpixel(int x,int y, int color)
 {
   unsigned int *ptr = (unsigned int*)screen->pixels;
   int lineoffset = y * (screen->pitch / 4);
   ptr[lineoffset + x] = color;
 }
+
+void putpixel(Coord *pt, int color)
+{
+  if (!pt)
+    return;
+  int x = pt->x;
+  int y = pt->y;
+  unsigned int *ptr = (unsigned int*)screen->pixels;
+  int lineoffset = y * (screen->pitch / 4);
+  ptr[lineoffset + x] = color;
+}
+
+bool practically_nothing(int value) {
+  const float some_value = 0.1;
+  value = value < 0 ? -value : value;
+  if (value < some_value) {
+    return true;
+  }
+  return false;
+};
+
+void line(Coord *ptA, Coord *ptB, int colour);
+void line (int startx, int starty, int stopx, int stopy, int colour);
+
+void line (int startx, int starty, int stopx, int stopy, int colour=0xff0000) {
+  Coord ptA,ptB;
+  ptA.x = startx;  ptA.y = starty;
+  ptB.x = stopx;  ptB.y = stopy;
+  return;
+}
+
+void line(Coord *ptA, Coord *ptB, int colour=0xff0000) {
+  int deltax = ptB->x - ptA->x;
+  int deltay = ptB->y - ptA->y;
+  
+  // if practically_nothing(dx):
+  //     y = starty
+  //     while dy > 0:
+  //         dy -= 1
+  //         yield (startx, y)
+  //         y += 1
+  //     while dy < 0:
+  //         dy += 1
+  //         yield (startx, y)
+  //         y -= 1
+
+  int startx = ptA->x;
+  int starty = ptA->y;
+  if (practically_nothing(deltax)) {
+    if (deltay < 0) {
+      deltay = -deltay;
+      startx = ptB->x;
+      starty = ptB->y;
+      // stopx = ptA->x;
+      // stopy = ptA->y;
+    }
+
+    int y = starty;
+    while(deltay > 0) {
+      deltay -= 1;
+      putpixel(startx, y,colour);
+      y += 1;
+    }
+    return;
+  }
+
+    // if practically_nothing(dy):
+    //     x = startx
+    //     while dx > 0:
+    //         dx -= 1
+    //         yield (x, starty)
+    //         x += 1
+            
+    //     while dx < 0:
+    //         dx += 1
+    //         yield (x, starty)
+    //         x -= 1
+
+  if (practically_nothing(deltay)) {
+    if (deltax < 0) {
+      deltax = -deltax;
+      startx = ptB->x;
+      starty = ptB->y;
+      // stopx = ptA->x;
+      // stopy = ptA->y;
+
+    }
+
+    int x = startx;
+    while(deltax > 0) {
+      deltax -= 1;
+      putpixel(x, starty,colour);
+      x += 1;
+    }    
+    return;
+  }
+        
+            
+  
+  return;
+}
 /*
 
-function line(x0, x1, y0, y1)
+ function line(x0, x1, y0, y1)
      int deltax := x1 - x0
      int deltay := y1 - y0
      real error := 0
@@ -35,86 +143,84 @@ function line(x0, x1, y0, y1)
          if error ≥ 0.5 then
              y := y + 1
              error := error - 1.0
-*/
-
-void line(int x0, int y0, int x1, int y1) {
-  float deltax = (float)(x1-x0);
-  float deltay = (float)(y1-y0);
-  
-  float d = fabs(deltay/deltax);
-  float err = 0;
-
-  int y = y0;
-  int x = x0;
-  
-  if (true) { //( d > 0 ) {
-    for (x = x0; x <= x1; x+=1) {
-      putpixel(x,y, 0xff0000);
-      err = err+d;    
-      if (err >= 0.5) {
-	y += 1;
-	err -= 1.0;
-      }
-    }
-  }
-  // else if ( d < 0 ) {
-  //   for (x = x0; x <= x1; x+=1) {
-  //     putpixel(x,y, 0xff0000);
-  //     err = err+d;    
-  //     if (err <= 0.5) {
-  // 	y -= 1;
-  // 	err -= 1.0;
-  //     }
-  //   }    
-  // }
-}
-
-
-void line2(int x0, int y0, int x1, int y1) {
-  float deltax = (float)(x1-x0);
-  float deltay = (float)(y1-y0);
-  
-  float d = deltay/deltax;
-  float err = 0;
-
-  int y = y0;
-  int x = x0;
-  
-  if ( d > 0 ) {
-    for (x = x0; x <= x1; x+=1) {
-      putpixel(x,y, 0xff0000);
-      err = err+d;    
-      if (err >= 0.5) {
-	y += 1;
-	err -= 1.0;
-      }
-    }
-  }
-  else if ( d < 0 ) {
-    for (x = x0; x <= x1; x+=1) {
-      putpixel(x,y, 0xff0000);
-      err = err+d;    
-      if (err <= -0.5) {
-  	y -= 1;
-  	err += 1.0;
-      }
-    }    
-  }
-}
 
 
 
-typedef struct Coord{
-  int x;
-  int y;
-};
+def line_skeleton(ptA,ptB,oct_x_dom=None,oct_y_dom=None):
+    '''
+    assumptions:
+    x is only increasing.
+    '''
+    dx,dy = dxdy(ptA,ptB)
+
+    startx, starty = ptA.x,ptA.y
+    stopx, stopy = ptB.x,ptB.y
+
+    if practically_nothing(dx):
+        y = starty
+        while dy > 0:
+            dy -= 1
+            yield (startx, y)
+            y += 1
+            
+        while dy < 0:
+            dy += 1
+            yield (startx, y)
+            y -= 1
+        
+            
+    if practically_nothing(dy):
+        x = startx
+        while dx > 0:
+            dx -= 1
+            yield (x, starty)
+            x += 1
+            
+        while dx < 0:
+            dx += 1
+            yield (x, starty)
+            x -= 1
+
+    if not practically_nothing(dy) and not practically_nothing(dx):
+
+        #import pdb; pdb.set_trace()
+        dx,dy = dxdy(ptA,ptB)
+        
+        startx, starty = ptA.x,ptA.y
+        stopx, stopy = ptB.x,ptB.y
+
+        points = []
+        if abs(dx) >= abs(dy):
+            if not oct_x_dom:
+                raise Exception("X dominant Line octants not implemented")
+
+            for pt in oct_x_dom(ptA,ptB):
+                yield pt
+        else:
+            if not oct_y_dom:
+                raise Exception("Y dominant Line octants not implemented")
+                
+            for pt in oct_y_dom(ptA,ptB):
+                yield pt
+ */
+
+
+
+
 
 static Coord coordlist[] = { 
   {10,10},
-  {200, 100}, 
-  {400,10},
-  {500,200}
+  {200, 10}, 
+  {200,200},
+  {10,200}
 };
+
+// static Coord coordlist[] = { 
+//   {10,10},
+//   {200, 100}, 
+//   {400,10},
+//   {500,200}
+// };
 
 void render()
 {   
@@ -136,7 +242,8 @@ void render()
   for (int i = 0; i  <  listlen; i++){
     i0 = i % listlen;
     i1 = (i+1)%listlen;
-    line2(coordlist[i0].x, coordlist[i0].y, coordlist[i1].x, coordlist[i1].y);
+    //line(coordlist[i0].x, coordlist[i0].y, coordlist[i1].x, coordlist[i1].y);
+    line(&coordlist[i0], &coordlist[i1]);
     putpixel(coordlist[i0].x, coordlist[i0].y, 0xffff00);
     putpixel(coordlist[i1].x, coordlist[i1].y, colourstart);
     //printf("%d, %d,:: %d %d",coordlist[i0].x, coordlist[i0].y, coordlist[i1].x, coordlist[i1].y);
