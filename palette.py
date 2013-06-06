1856b3a5e8105b67a37dc9bcb50dccdb40055cd9
Stephan van Ellewee <stephan.van.ellewee@gmail.com>
2013-06-06 05:43:23 +0200

    fixing palette code in prep for lighting poly

--- palette.py
+++ palette.py
@@ -16,7 +16,8 @@ if True:
 else:
     width,height = DISPLAY_WIDTH, DISPLAY_HEIGHT
 
-surface = pygame.Surface((width,height),0,8)
+fsurface = pygame.Surface((width,height),0,8)
+farray = numpy.zeros((width, height))
 
 class ModelColor (object):
     red = 0
@@ -29,24 +30,30 @@ gamma64_levs = [ 0, 10, 14, 17, 19, 21, 23, 24, 26, 27, 28, 29, 31, 32, 33, 34,
                  47, 48, 48, 49, 49, 50, 51, 51, 52, 52, 53, 53, 54, 54, 55, 55,
                  56, 56, 57, 57, 58, 58, 59, 59, 60, 60, 61, 61, 62, 62, 63, 63];
 
-pblock = {}
-def init_palette():
-    global pblock
+def set_palette(surface):
+    cmap = numpy.zeros((256,3))
+    red,green,blue = 0,1,2
+
     for r in range(0,4):
         for g in range(0,4):
             for b in range(0,4):
                 idx = (r<<4)+(g<<2)+b;
-                #print r,g,b,hex(idx),idx
-                pblock.update({idx: [gamma4_levs[r], gamma4_levs[g], gamma4_levs[b]] })
-        for r in range(0,64):
-            pblock.update({64+r: [gamma64_levs[r], 0,0] })
+                cmap[idx, :]= gamma4_levs[r], gamma4_levs[g], gamma4_levs[b]
 
-        for g in range(0,64):
-            pblock.update({128+g: [0, gamma64_levs[g], 0] })
+    for r in range(0,64):
+        cmap[64+r, :] = gamma64_levs[r], 0,0
 
-        for b in range(0,64):
-            pblock.update({192+b: [0,0,gamma64_levs[b]] })
+    for g in range(0,64):
+        cmap[128+g, :] = 0, gamma64_levs[g], 0
 
+    for b in range(0,64):
+        cmap[192+b, :] = 0,0,gamma64_levs[b]
+
+    surface.set_palette(cmap)
+
+    # for i in cmap:
+    #     print i
+        
 def model_colour_to_colour_index(color):
     if color.red == 0:
         if color.green ==0:
@@ -61,14 +68,23 @@ def model_colour_to_colour_index(color):
             ((color.green & 0xC0) >>4)|
             ((color.blue & 0xC0) >> 6))
 
-init_palette()
-for i in pblock.items():
-    print i
-
 print len(gamma64_levs)
 
 def render():
-    pass
+    global farray
+    global fsurface
+    width, height = farray.shape
+    temp_array = numpy.zeros((width, height))    
+
+    col = 0
+    for x in range(0,width):
+        for y in range(0,height):
+            temp_array.itemset(x,y,col) 
+        col += 1
+
+    farray = temp_array
+    pygame.surfarray.blit_array(fsurface,farray )    
+
 
 random.seed()
 pygame.init()
@@ -77,10 +93,11 @@ screen.set_alpha(None)
 clock = pygame.time.Clock()
 
 def main():
+    global fsurface
     running = True
-    #set_palette(fire_surface)
+    set_palette(fsurface)
     pygame.event.set_allowed([QUIT, KEYDOWN, KEYUP])
-    t = True
+    t = True    
     while running:
         for event in pygame.event.get():
             if event.type == KEYDOWN or event.type==KEYUP: #QUIT:
@@ -94,8 +111,7 @@ def main():
         '''
         to see the original image
         '''
-        screen.blit(surface, (0,0))  
-
+        screen.blit(fsurface, (0,0))  
 
         pygame.display.flip()
 
@@ -103,3 +119,4 @@ def main():
   
 if __name__=='__main__':
     main()
+    
