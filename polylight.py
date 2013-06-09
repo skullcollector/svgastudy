847f3d5d43ab32ca36464ca9bce72bfffdd77789
Stephan van Ellewee <stephan.van.ellewee@gmail.com>
2013-06-09 18:49:54 +0200

    cant make light work. shelving it until later

--- polylight.py
+++ polylight.py
@@ -25,7 +25,7 @@ PROJECTION_RATIO =-5.0
 SCREEN_WIDTH, SCREEN_HEIGHT = 1024, 800
 
 
-class ModelColor (object):
+class ModelColour (object):
     red = 0
     green = 0
     blue = 0
@@ -69,22 +69,6 @@ def set_palette(surface):
         cmap[192+b, :] = 0,0,gamma64_levs[b]
 
     surface.set_palette(cmap)
-
-# From L55-3.c        
-def model_colour_to_colour_index(color):
-    if color.red == 0:
-        if color.green ==0:
-            return 192+(color.blue >> 2)
-        elif color.blue ==0:
-            return 128+(color.green >> 2)
-
-    elif color.green ==0 and color.blue ==0:
-        return 64+(color.red >> 2)
-
-    return (((color.red & 0xC0) >>2)|
-            ((color.green & 0xC0) >>4)|
-            ((color.blue & 0xC0) >> 6))
-
 
 
 class PygamePlotter(Plotter):
@@ -157,10 +141,12 @@ def isbackface(polypts):
     return xproduct > 0
 
 def xform_and_project_poly(surface, xform4X4, polypts3d, colour = 0x00ff00,draw_hlines=False):
+
     plt = PygamePlotter(surface,default_colour=colour)
     polypts2d = []
     if True:
         txpolypts_array = []
+        # turn points from world(object) space to view space.
         for pt in polypts3d:
             txpolypts_array.append(xformvec(xform4X4,pt))
         #if not isbackface(txpolypts_array):
@@ -172,6 +158,7 @@ def xform_and_project_poly(surface, xform4X4, polypts3d, colour = 0x00ff00,draw_
             if the 3d point goes to the left or right it moves by by xval/zval (zval larger, perceived xposition further away)
             if the 3d point goes to up or down it moves by by yval/zval.
             '''
+            # turn view 3D space to screen "2D space"
             new_x = int(round((1.0*xval/zval * 1.0  * PROJECTION_RATIO*(SCREEN_WIDTH/2.0)+0.5) + SCREEN_WIDTH/2)) if zval != 0 else xval
             new_y = int(round((1.0*yval/zval * -1.0 * PROJECTION_RATIO*(SCREEN_WIDTH/2.0)+0.5) + SCREEN_HEIGHT/2)) if zval != 0 else yval
             polypts2d.append(Coord(new_x,new_y))  
@@ -180,6 +167,9 @@ def xform_and_project_poly(surface, xform4X4, polypts3d, colour = 0x00ff00,draw_
     else:
         polypts2d = map(partial(gen_proj_pt,xform4X4=xform4X4), polypts3d)
     return fill_convex_poly(polypts2d,drawer=plt,colour=colour,draw_hlines=draw_hlines),isbackface(txpolypts_array)
+
+
+model_intensity = ModelIntensity(0.0,0.1,0.0)
 
 spot0 = {'direction': numpy.array([ [-1], [-1], [-1], [1]]),
          'intensity': numpy.array([ [0.0], [0.75], [0.0], [1]]),}
@@ -194,7 +184,8 @@ spot_direction_view = [ [] for i in spot_data ]
 
 # #define DOT_PRODUCT(V1,V2) \
 #    (FixedMul(V1.X,V2.X)+FixedMul(V1.Y,V2.Y)+FixedMul(V1.Z,V2.Z))
-dot_product =lambda v1,v2: v1.x*v2.x + v1.y*v2.y + v1.z*v2.z
+#dot_product =lambda v1,v2: v1.x*v2.x + v1.y*v2.y + v1.z*v2.z
+dot_product =lambda v1,v2: v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2]
 
 def set_spot_intensity(spot_idx, intensity):
     global spot_data
@@ -217,6 +208,108 @@ def set_spot_direction(spot_idx,  xform4X4):
     spot_direction_view[spot_idx][1] *= -1
     spot_direction_view[spot_idx][2] *= -1
     #print spot_direction_world, spot_direction_view
+
+def intensity_adj_col(colour_model, intensity_model):
+    returnvalue = ModelColour(0,0,0)
+    intround = lambda x : int(round(x))
+    returnvalue.red = min( intround(colour_model.red * intensity_model.red), 255)
+    returnvalue.green = min( intround(colour_model.green * intensity_model.blue), 255)
+    returnvalue.blue = min( intround(colour_model.blue * intensity_model.green), 255)
+    return returnvalue
+
+
+# From L55-3.c        
+def model_colour_to_colour_index(colour):
+    if colour.red == 0:
+        if colour.green ==0:
+            return 192+(colour.blue >> 2)
+        elif colour.blue ==0:
+            return 128+(colour.green >> 2)
+
+    elif colour.green ==0 and colour.blue ==0:
+        return 64+(colour.red >> 2)
+
+    return (((colour.red & 0xC0) >>2)|
+            ((colour.green & 0xC0) >>4)|
+            ((colour.blue & 0xC0) >> 6))
+
+def get_normal(pts, baseidx, vec0, vec1):
+    v1 = pts[vec0][0] - pts[baseidx][0]
+    v2 = pts[vec0][1] - pts[baseidx][1]
+    v3 = pts[vec0][2] - pts[baseidx][2]
+
+    w1 = pts[vec1][0] - pts[baseidx][0]
+    w2 = pts[vec1][1] - pts[baseidx][1]
+    w3 = pts[vec1][2] - pts[baseidx][2]
+
+
+    cross_product = numpy.array([[0],[0],[0],[1]])
+    cross_product[0] = v2*w3 - v3*w2
+    cross_product[1] = v3*w1 - v1*w3
+    cross_product[2] = v1*w2 - v2*w1
+    
+    xlen = -(cross_product[0] - pts[baseidx][0])
+    ylen = -(cross_product[1] - pts[baseidx][1])
+    zlen = -(cross_product[2] - pts[baseidx][2])
+    length = numpy.sqrt(xlen*xlen + ylen*ylen + zlen*zlen)
+    
+    cross_product[0] = pts[baseidx][0] + 1.0*xlen/length
+    cross_product[1] = pts[baseidx][1] + 1.0*ylen/length
+    cross_product[2] = pts[baseidx][2] + 1.0*zlen/length
+
+    return cross_product
+
+def xform_and_project_poly_with_light(surface, xform4X4, polypts3d, colour = 0x00ff00,draw_hlines=False):
+
+    model_colour = ModelColour(0xff,0xff,0xff)
+
+    plt = PygamePlotter(surface,default_colour=colour)
+    polypts2d = []
+    if True:
+        txpolypts_array = []
+        # turn points from world(object) space to view space.
+        for pt in polypts3d:
+            txpolypts_array.append(xformvec(xform4X4,pt))
+
+        set_spot_direction(0,xform4X4)
+        set_spot_intensity(0,model_intensity)
+        
+        set_spot_direction(1,xform4X4)
+        set_spot_intensity(1,model_intensity)
+        
+        set_spot_direction(2,xform4X4)
+        set_spot_intensity(2,model_intensity)
+
+        
+        normal = get_normal(txpolypts_array, 0, 1, 2)  # probably need to be cleverer for non triangles.
+        for spot in spot_direction_view[1:]:
+            #print spot,'....', list(spot_direction_view
+            dot_prod = dot_product(normal,spot)
+            #print dot_prod,'..'
+            if dot_prod > 0:
+                #print '----',spot_direction_view,dot_prod;
+                model_intensity.red += spot[0]*dot_prod;
+                model_intensity.green += spot[1]*dot_prod;
+                model_intensity.blue += spot[2]*dot_prod;
+                model_colour = intensity_adj_col(model_colour,model_intensity)
+
+        for txpolypt in txpolypts_array:
+            xval,yval,zval,wval = txpolypt
+            '''
+            so far theory is:
+            we start at center point x,y = SCREEN_WIDTH/2, SCREEN_HEIGHT/2
+            if the 3d point goes to the left or right it moves by by xval/zval (zval larger, perceived xposition further away)
+            if the 3d point goes to up or down it moves by by yval/zval.
+            '''
+            # turn view 3D space to screen "2D space"
+            new_x = int(round((1.0*xval/zval * 1.0  * PROJECTION_RATIO*(SCREEN_WIDTH/2.0)+0.5) + SCREEN_WIDTH/2)) if zval != 0 else xval
+            new_y = int(round((1.0*yval/zval * -1.0 * PROJECTION_RATIO*(SCREEN_WIDTH/2.0)+0.5) + SCREEN_HEIGHT/2)) if zval != 0 else yval
+            polypts2d.append(Coord(new_x,new_y))  
+
+    return fill_convex_poly(polypts2d,
+                            drawer=plt,
+                            colour=0x00ff00, #model_colour_to_colour_index(colour),
+                            draw_hlines=draw_hlines),model_colour,isbackface(txpolypts_array)
 
 def render(surface,rotation=0, new_hotness=True):
     vertices = [
@@ -249,23 +342,23 @@ def render(surface,rotation=0, new_hotness=True):
 
     worldviewxform = concat_x_forms(worldform, polyform)
     draw_hlines = False if new_hotness else True
-    set_spot_direction(0,worldviewxform)
-    hlinesdata,is_behind_poly = xform_and_project_poly(surface, worldviewxform, vertices, draw_hlines=draw_hlines)
+    hlinesdata,colour,is_behind_poly = xform_and_project_poly_with_light(surface, worldviewxform, vertices, draw_hlines=draw_hlines)
     if not draw_hlines:
         vals = hlinesdata.gettuples()
         temp_array = numpy.zeros((SCREEN_WIDTH, SCREEN_HEIGHT))   
         '''
         stil calculates polys even if not facing... bad? unnecesary?
         '''
-        color = ModelColor(255,255,255)
+        #colour = ModelColour(255,255,255)
         if not is_behind_poly:
+            #unit_normal = 
             y = hlinesdata.ystart
             for v in vals:
                 x1, x2 = v
                 if x1 > x2:                
-                    temp_array[x2:x1,y].fill(model_colour_to_colour_index(color))
+                    temp_array[x2:x1,y].fill(model_colour_to_colour_index(colour))
                 else:
-                    temp_array[x1:x2,y].fill(model_colour_to_colour_index(color))
+                    temp_array[x1:x2,y].fill(model_colour_to_colour_index(colour))
                 y += 1
         pygame.surfarray.blit_array(surface,temp_array)
 
@@ -273,6 +366,7 @@ clock = pygame.time.Clock()
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT),0,8)
+
 
     running = True
     rotation = 0
